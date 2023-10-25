e8349feffd246466b7306d522a91e6ee273b7428
Denis CARLUS <denis.carlus@touchline3d.com>
2023-10-25 21:31:51 +0200

    New animations + new input system

--- Assets/Scripts/AnimationStateController.cs
+++ Assets/Scripts/AnimationStateController.cs
@@ -1,6 +1,4 @@
 using Assets.Scripts.AppStrings;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
@@ -9,31 +7,66 @@ using UnityEngine;
 public class AnimationStateController : MonoBehaviour
 {
     Animator m_animator;
-    float m_velocityXValue = 0f;
-    float m_velocityZValue = 0f;
+    // ID references to the animator variables.
+    int m_velocityMagnitudeHash;
+    int m_velocityXHash;
+    int m_velocityZHash;
 
-    // Start is called before the first frame update
+    PlayerInputs m_playerInputs;
+    Vector2 m_currentMovement;
+    //bool m_walkPressed;
+
+    // Called even before Start().
+    void Awake()
+    {
+        m_playerInputs = new PlayerInputs();
+        m_playerInputs.CharacterControls.Movement.performed += ctx =>
+        {
+            m_currentMovement = ctx.ReadValue<Vector2>();
+        };
+        //m_playerInputs.CharacterControls.Walk.performed += ctx => m_walkPressed = ctx.ReadValueAsButton();
+    }
+
+    // Start is called before the first frame update.
     void Start()
     {
         m_animator = GetComponent<Animator>();
+        m_velocityMagnitudeHash = Animator.StringToHash(AnimationParameters.VelocityMagnitude);
+        m_velocityXHash = Animator.StringToHash(AnimationParameters.VelocityX);
+        m_velocityZHash = Animator.StringToHash(AnimationParameters.VelocityZ);
     }
 
-    // Update is called once per frame
+    // Update is called once per frame.
     void Update()
     {
-        var horizontalSpeed = Input.GetAxis(Inputs.Horizontal);
-        var verticalSpeed = Input.GetAxis(Inputs.Vertical);
+        handleMovements();
+    }
 
-        var velocityVector = new Vector3(horizontalSpeed, verticalSpeed, 0f);
+    void handleMovements()
+    {
+        var currentMovement = m_currentMovement;
+
+        var velocityVector = new Vector3(currentMovement.x, 0f, currentMovement.y);
         velocityVector.Normalize();
         var velocityMagnitude = velocityVector.magnitude;
-        m_animator.SetFloat(AnimationParameters.VelocityMagnitude, velocityMagnitude);
+        m_animator.SetFloat(m_velocityMagnitudeHash, velocityMagnitude);
 
-        // In case of improvements, values are stored in class member variables.
-        m_velocityXValue = horizontalSpeed;
-        m_velocityZValue = verticalSpeed;
+        //if (m_walkPressed)
+        //{
+        //    currentMovement /= 2f;
+        //}
 
-        m_animator.SetFloat(AnimationParameters.VelocityX, m_velocityXValue);
-        m_animator.SetFloat(AnimationParameters.VelocityZ, m_velocityZValue);
+        m_animator.SetFloat(m_velocityXHash, currentMovement.x);
+        m_animator.SetFloat(m_velocityZHash, currentMovement.y);
+    }
+
+    void OnEnable()
+    {
+        m_playerInputs.CharacterControls.Enable();
+    }
+
+    void OnDisable()
+    {
+        m_playerInputs.CharacterControls.Disable();
     }
 }
